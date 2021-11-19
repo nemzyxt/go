f0bdff856d8d8dd7791fe8ff1299130c9eb5d1b9
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-11-19 15:25:48 +0300

    Add files via upload

---
+++ hello.go
@@ -0,0 +1,10 @@
+//Author : Nemuel Wainaina
+//This is a simple program that prints out the text "Hello world!" on the console
+
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Hello world!")
+}
