b6834163402d67b27840b77576501d2f5772f4a8
nemzyxt <94122064+nemzyxt@users.noreply.github.com>
2021-11-19 15:27:59 +0300

    Add files via upload

---
+++ numbers.go
@@ -0,0 +1,15 @@
+//Author : Nemuel Wainaina
+//This is a simple program to show some arithmetic operations on Integers data type
+
+package main
+
+import "fmt"
+
+func main(){
+    fmt.Println("Numbers in Go : ")
+    fmt.Println("Addition :\n  8 + 2 = ", 8+2)
+    fmt.Println("Subtraction : \n  8 - 2 = ", 8-2)
+    fmt.Println("Division :\n  8 / 2 = ", 8/2)
+    fmt.Println("Multiplication : \n  8 * 2 = ", 8*2)
+    fmt.Println("Modulus(Remainder) : \n  8 % 2 = ", 8%2)
+}
